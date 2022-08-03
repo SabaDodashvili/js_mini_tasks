c101249691564cc591d62af785853fbc158e53aa
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-03 19:23:21 +0400

    add task 14

--- arrays.js
+++ arrays.js
@@ -254,3 +254,36 @@ let users = [vasya, petya, masha];
 let names = users.map(el => el.name);
 
 console.log(names);
+
+// 14) У вас есть массив объектов user, и у каждого из объектов есть name, surname и id. Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+//РЕШЕНИЕ-1
+let vasya = { name: 'Вася', surname: 'Пупкин', id: 1 };
+let petya = { name: 'Петя', surname: 'Иванов', id: 2 };
+let masha = { name: 'Маша', surname: 'Петрова', id: 3 };
+
+let users = [vasya, petya, masha];
+
+let usersMapped = users.map(el => {
+	let newObj = {};
+	newObj['fullName'] = `${el['name']} ${el['surname']}`;
+	newObj['id'] = el['id'];
+
+	return newObj;
+});
+
+console.log(usersMapped);
+
+//РЕШЕНИЕ-2
+let vasya = { name: 'Вася', surname: 'Пупкин', id: 1 };
+let petya = { name: 'Петя', surname: 'Иванов', id: 2 };
+let masha = { name: 'Маша', surname: 'Петрова', id: 3 };
+
+let users = [vasya, petya, masha];
+
+let usersMapped = users.map(el => ({
+	fullName: `${el.name} ${el.surname}`,
+	id: el.id,
+}));
+
+console.log(usersMapped);
