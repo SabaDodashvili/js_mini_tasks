a35c3461a60122aa190635a9429fb920cecc9151
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-04 18:53:19 +0400

    add task 3,4

--- objects.js
+++ objects.js
@@ -27,3 +27,41 @@ function isEmpty(obj) {
 }
 
 console.log(isEmpty(schedule));
+
+// 3) We have an object storing salaries of our team:
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+// If salaries is empty, then the result must be 0.
+
+let salaries = {
+	John: 100,
+	Ann: 160,
+	Pete: 130,
+};
+
+let sum = 0;
+
+for (const key in salaries) {
+	sum += salaries[key];
+}
+
+console.log(sum);
+
+// 4) Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+// P.S. Use typeof to check for a number here.
+
+let menu = {
+	width: 200,
+	height: 300,
+	title: 'My menu',
+};
+
+function multiplyNumeric(obj) {
+	for (const key in obj) {
+		if (typeof obj[key] == 'number') obj[key] *= 2;
+	}
+}
+
+multiplyNumeric(menu);
+
+console.log(menu);
